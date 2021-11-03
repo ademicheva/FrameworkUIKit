e3a70c06b1cae22b5474ee0a1623609972c183b6
Alina Demicheva <ademicheva@gmail.com>
2021-11-04 00:38:02 +0200

    add basic functionality of UIButton an UILabel

--- FrameworkUIKit/ViewController.swift
+++ FrameworkUIKit/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: Label
+        label.isHidden = true // скрываем лейбл
+        label.font = label.font.withSize(35) // меняем размер шрифта
+        label.textColor = .red // меняем цвет шрифта (.red == UICoLor.red)
+        
+        // MARK: button
+        button.setTitle("Get result", for: .normal) // меняем заголовок кнопки
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .green
     }
 
-
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        if label.isHidden {
+            label.text = "Hello World!"
+            
+            button.setTitle("Clear", for: .normal) // меняем заголовок кнопки
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .red
+            label.isHidden = false
+            
+            
+        } else {
+            
+            button.setTitle("Get result", for: .normal) // меняем заголовок кнопки
+            button.setTitleColor(.blue, for: .normal)
+             button.backgroundColor = .green
+            label.isHidden = true
+            
+            
+            
+        }
+    }
+    
 }
 
