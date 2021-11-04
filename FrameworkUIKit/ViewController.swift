999a51147e6d5033d1bc11869becff4c9aaea121
Alina Demicheva <ademicheva@gmail.com>
2021-11-04 14:50:37 +0200

    add basic uitextview

--- FrameworkUIKit/ViewController.swift
+++ FrameworkUIKit/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        button.setTitle("Готово!", for: .normal)
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        label.isHidden = false
+        guard textField.text?.isEmpty == false else { return }
+        
+        if let _ = Double(textField.text!) {
+            let alert = UIAlertController(title: "Wrong format", message: "Please enter your name", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok!", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            textField.text = nil
+        } else {
+            label.text = textField.text
+            textField.text = nil
+        }
+    }
 }
-
